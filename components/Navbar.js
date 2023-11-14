c754020ea5cba0b7697d272edf34291b3107d818
AlfalahOdn <alfalahokdinda10@gmail.com>
2023-11-15 01:00:46 +0700

    OP-23 Added Hero and Footer Section

--- components/Navbar.js
+++ components/Navbar.js
@@ -11,27 +11,27 @@ export default function Navbar() {
     };
   return (
     <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">   
+        <div className="max-w-10xl mx-auto px-4 sm:px-6 lg:px-8">   
             <div className="flex items-center justify-between h-16">
                 <div className="flex item-center">
                     <div className="flex-shrink-0">
-                        <Link href="/" className="text-white font-semibold">
+                        <Link href="/" className="text-white font-semibold lg:text-2xl md:text-xl">
                             Barvalabs
                         </Link>
         
                     </div>
                 </div>
                 <div className="hidden md:block">
-                     <div className="ml-4 mr-4 flex items-center space-x-4">
-                        <a className="text-white hover:bg-white hover:text-black rounded-lg p-2">
-                            <Link href="/">Home</Link>
-                        </a>
-                        <a className="text-white hover:bg-white hover:text-black rounded-lg p-2">
-                            <Link href="/">About Us</Link>
-                        </a>
-                        <a className="text-white hover:bg-white hover:text-black rounded-lg p-2">
-                             <Link href="/">Contact</Link>
-                        </a>
+                     <div className="ml-4 mr-4 flex items-center space-x-4 lg:text-xl">
+                        <Link href="/">
+                            <div className="text-white hover:bg-white hover:text-black rounded-lg p-2">Home</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="text-white hover:bg-white hover:text-black rounded-lg p-2">Contact</div>
+                        </Link>
+                        <Link href="/">
+                            <div className="text-white hover:bg-white hover:text-black rounded-lg p-2">About Us</div>
+                        </Link>
                 </div>
                 </div>
                 <div className="md:hidden flex items-center">
@@ -78,15 +78,15 @@ export default function Navbar() {
         {isClick && (
             <div className="md:hidden ">
                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <a className="text-white block hover:bg-white hover:text-black rounded-lg p-2">
+                <div className="text-white block hover:bg-white hover:text-black rounded-lg p-2">
                     <Link href="/">Home</Link>
-                 </a>
-                <a className="text-white block hover:bg-white hover:text-black rounded-lg p-2">
+                 </div>
+                <div className="text-white block hover:bg-white hover:text-black rounded-lg p-2">
                     <Link href="/">About Us</Link>
-                 </a>
-                 <a className="text-white block hover:bg-white hover:text-black rounded-lg p-2">
+                 </div>
+                 <div className="text-white block hover:bg-white hover:text-black rounded-lg p-2">
                     <Link href="/">Contact</Link>
-                 </a>
+                 </div>
                 </div>
             </div>
         )}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,21 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Container } from 'postcss'
-import Navbar from '/components/Navbar'
+import 'tailwindcss/tailwind.css'
+import Navbar from 'components/Navbar'
+import Hero from 'components/Hero'
+import About from 'components/About'
+
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className={`flex min-h-screen flex-col bg-[#121212]`}>
-      <Navbar/>
-      <div className=' container mx-auto px-12 py-4'>
-      </div>
-    </main>
+    <div className={`flex min-h-screen flex-col bg-[#121212]`}>
+
+      <Navbar />
+      <Hero />
+      <About />
+    </div>
+    
   )
 }
